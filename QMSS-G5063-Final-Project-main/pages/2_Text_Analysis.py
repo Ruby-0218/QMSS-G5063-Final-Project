f57ce8c6d83e287bbb1b3f60e2a5641258a2bd87
Ju-Ting Sun (Ruby) <79563856+Ruby-0218@users.noreply.github.com>
2026-05-03 14:56:33 -0400

    Update 2_Text_Analysis.py

--- QMSS-G5063-Final-Project-main/pages/2_Text_Analysis.py
+++ QMSS-G5063-Final-Project-main/pages/2_Text_Analysis.py
@@ -19,6 +19,9 @@ st.markdown("""
     [data-testid="stSidebarNav"] ul li:first-child {
         display: none;
     }
+    [data-testid="stSidebar"] hr {
+        display: none !important;
+    }
 
     .block-container {
         padding-top: 3rem;
